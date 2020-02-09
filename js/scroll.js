9d36d7873eee6a496da37fc1e314979d28c50dcc
Matt Drapchaty <Drapchaty@comcast.net>
2020-02-08 22:13:45 -0700

    nav bar fixed, smoother scroll working

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+$(function() {
+
+//Smooth scrolling on link click
+    $(document).on('click', 'a[href^="#"]', function (event) {
+      event.preventDefault();
+
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 500);
+    });
+
+
+
+});
